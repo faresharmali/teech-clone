83585010cbcbdaa4629811c0bd5cd07d7a71e503
fares <faresharmali3@gmail.com>
2024-05-07 18:37:01 +0200

    feat: adding favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Teech clone",
   description: "Teech clone",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 
