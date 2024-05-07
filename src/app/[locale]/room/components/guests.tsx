d5fbb5ee62b159265aac39d352105cb4bffbad73
fares <faresharmali3@gmail.com>
2024-05-07 22:01:39 +0200

    fix: no stream case

--- src/app/[locale]/room/components/guests.tsx
+++ src/app/[locale]/room/components/guests.tsx
@@ -10,23 +10,28 @@ import Guest5 from "@/assets/images/guests/guest5.png";
 interface IGuestCard {
   name: string;
   img: StaticImageData;
+  isStreaming: boolean;
 }
 const guestList: IGuestCard[] = [
   {
     name: "Alex",
     img: Guest1,
+    isStreaming: true,
   },
   {
     name: "Martha",
     img: Guest2,
+    isStreaming: true,
   },
   {
     name: "Anna",
+    isStreaming: false,
     img: Guest3,
   },
   {
     name: "Leroy",
     img: Guest4,
+    isStreaming: true,
   },
 ];
 
@@ -35,20 +40,31 @@ const Guests = () => {
     <div className="w-[15rem] h-full bg-[#191D21] rounded-md flex flex-col items-center justify-between gap-4 py-4">
       <div className="flex flex-col items-center w-full gap-2">
         {guestList.map((section) => (
-          <GuestCard key={section.name} name={section.name} img={section.img} />
+          <GuestCard
+            key={section.name}
+            name={section.name}
+            img={section.img}
+            isStreaming={section.isStreaming}
+          />
         ))}
       </div>
-      <GuestCard name={"Me"} img={Guest5} />
+      <GuestCard name={"Me"} img={Guest5} isStreaming={true} />
     </div>
   );
 };
 
 export default Guests;
 
-const GuestCard = ({ name, img }: IGuestCard) => {
+const GuestCard = ({ name, img, isStreaming }: IGuestCard) => {
   return (
     <div className="flex items-center gap-2 h-[120px] border border-1 border-gray-500 bg-blue-500 relative w-[90%] rounded-md overflow-hidden">
-      <Image alt="cam" src={img} objectFit="cover" fill />
+      {isStreaming ? (
+        <Image alt="cam" src={img} objectFit="cover" fill />
+      ) : (
+        <div className="bg-purple-500 w-full h-full flex justify-center items-center">
+          <h1 className="text-3xl text-white">A</h1>
+        </div>
+      )}
       <div className="absolute bottom-1 left-1 bg-[#2C3138] rounded-md opacity-90">
         <h2 className="text-white p-1 text-sm ">{name}</h2>
       </div>
